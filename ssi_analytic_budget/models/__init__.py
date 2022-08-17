132ae405f5b40cf6c40da906e0c9b29cb5f6d509
Andhitia Rama <andhitia.r@gmail.com>
2022-08-17 16:17:18 +0700

    [ssi_analytic_budget] 14.0.1.0.0

---
+++ ssi_analytic_budget/models/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from . import (
+    analytic_budget_type,
+    analytic_budget_type_account,
+    analytic_budget_budget,
+    analytic_budget_realization,
+    analytic_budget_detail,
+    analytic_budget_realization_budgeted,
+    analytic_budget_realization_unbudgeted_helper,
+    analytic_budget_realization_unbudgeted,
+)
