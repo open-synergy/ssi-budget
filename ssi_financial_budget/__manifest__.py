b7e195a25b0def1da8456b3db37056f93e1b8c4b
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-19 10:05:51 +0700

    [ssi_financial_budget] 11.0.1.0.0

---
+++ ssi_financial_budget/__manifest__.py
@@ -0,0 +1,40 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+{
+    "name": "Financial Budget",
+    "version": "11.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+        "ssi_master_data_mixin",
+        "ssi_product_line_price_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_transaction_terminate_mixin",
+        "ssi_company_currency_mixin",
+        "account_fiscal_month",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/approval_template_data.xml",
+        "data/policy_template_data.xml",
+        "menu.xml",
+        "views/financial_budget_type_views.xml",
+        "views/financial_budget_budget_views.xml",
+    ],
+    "demo": [
+        "demo/budget_type_demo.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
---
+++ ssi_financial_budget/models/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from . import (
+    financial_budget_type,
+    financial_budget_budget,
+    financial_budget_detail,
+    financial_budget_detail_summary,
+    financial_budget_move_line,
+    financial_budget_move_line_summary,
+    financial_budget_account,
+    financial_budget_summary,
+)
